be70289de7350c7be389b20807b98a7cd18de046
Sasha <example@gmail.com>
2023-07-04 18:56:50 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,29 @@
+const obj = {
+    1: 'j',
+    '2': 'male',
+    v: 1,
+    abc: {
+        def: {
+           
+        }
+    }
+};
+let r = [1, 'tr', {}, [1,2]];
+
+let b = 'name';
+obj[b] = 'new';
+
+console.log(obj[b]);
+
+const storeName = 'testName';
+const storeDescription = {
+    budget: 10000,
+    employees: ['John', 'Land', 'Yozef'],
+    products: {
+        'orange': 10,
+        'banana': 5
+    },
+    open: true
+};
+
+console.log(storeDescription.products['banana'])
